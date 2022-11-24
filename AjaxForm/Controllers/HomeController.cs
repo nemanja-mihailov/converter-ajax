be025325099a59c4af4f7334734c8663888d7064
nemanja-mihailov <nemanjamihailov0@gmail.com>
2022-11-24 10:53:37 +0100

    first commit

---
+++ AjaxForm/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using AjaxForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxForm.Controllers
+{
+    public partial class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View(new CoModel());
+        }
+        [HttpPost]
+        public double ConvertToEuro(CoModel coModel)
+        {
+            double euro = coModel.broj / 118;
+            return euro;
+        }
+
+        [HttpPost]
+        public double ConvertToDinar(CoModel coModel)
+        {
+            double dinar = coModel.broj * 118;
+            return dinar;
+        }
+    }
+}
---
+++ AjaxForm/Models/CoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxForm.Models
+{
+    public class CoModel
+    {
+        public double broj { get; set; }
+
+        public double dinar { get; set; }
+
+        public double euro { get; set; }
+    }
+}
+    
+    
